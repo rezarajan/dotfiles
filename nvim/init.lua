9a5d48380bde84517465811ca65a9d385f485332
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2025-03-09 03:00:50 -0400

    (nvim) use gruvbox theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -789,24 +789,55 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'maxmx03/solarized.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'maxmx03/solarized.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     require('solarized').setup {
+  --       variant = 'winter', -- You can choose "spring", "summer", "autumn", or "winter"
+  --     }
+  --     -- Load the colorscheme here.
+  --     vim.o.background = 'light'
+  --     vim.cmd.colorscheme 'solarized'
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    -- Default options:
     init = function()
-      require('solarized').setup {
-        variant = 'winter', -- You can choose "spring", "summer", "autumn", or "winter"
-      }
-      -- Load the colorscheme here.
-      vim.o.background = 'light'
-      vim.cmd.colorscheme 'solarized'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+      require('gruvbox').setup {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = '', -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    }
+    vim.cmd.colorscheme 'gruvbox'
+  end,
   },
 
   -- { -- You can easily change to a different colorscheme.
