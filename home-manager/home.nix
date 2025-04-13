51226eb75a61df9ff92f366b0d2aed53f1e03694
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2025-04-13 14:01:34 -0400

    (dotfiles) update home-manager and nvim locks - NOTE: home-manager using nvidia, switch as necessary

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,7 +7,7 @@
   nixGL.packages = nixgl.packages;
   # see: https://mynixos.com/home-manager/option/nixGL.defaultWrapper
   # NOTE: for nvidia, the --impure option must be passed to home-manager switch
-  nixGL.defaultWrapper = "mesa"; # options one of "mesa", "mesaPrime", "nvidia", "nvidiaPrime"
+  nixGL.defaultWrapper = "nvidia"; # options one of "mesa", "mesaPrime", "nvidia", "nvidiaPrime"
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
