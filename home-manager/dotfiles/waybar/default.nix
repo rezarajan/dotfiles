a773000e87df43a8938a456bcf1f40319a66cc9d
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2025-05-14 14:00:27 +0000

    (home-manager) fix permissions on toggle.sh to be executable - created a shell script bin derivation for this

--- home-manager/dotfiles/waybar/default.nix
+++ home-manager/dotfiles/waybar/default.nix
@@ -1,10 +1,14 @@
 { config, pkgs, ... }:
 
+let
+  # Define the source of the script with variables to be replaced
+  toggleScript = pkgs.writeShellScriptBin "toggle.sh" (
+    pkgs.replaceVars ./toggle.sh {
+      theme = "catppuccin";  # Theme to be replaced
+      waybar = pkgs.lib.getExe pkgs.waybar;  # Waybar executable path
+    });
+in
 {
   home.file.".config/waybar/themes".source = ./themes;
-  home.file.".config/waybar/toggle.sh".source = 
-    pkgs.replaceVars ./toggle.sh{
-      theme = "catppuccin";
-      waybar = pkgs.lib.getExe pkgs.waybar;
-    };
+  home.file.".config/waybar/toggle.sh".source = pkgs.lib.getExe toggleScript;
 }
