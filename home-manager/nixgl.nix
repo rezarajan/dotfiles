eec328fdd0d2e551413fc08f88d38d2196d745dd
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2025-05-19 23:19:39 +0000

    (hyprland) change browser launch to flatpak Zen

--- home-manager/nixgl.nix
+++ home-manager/nixgl.nix
@@ -7,6 +7,6 @@
   nixGL.packages = nixgl.packages;
   # see: https://mynixos.com/home-manager/option/nixGL.defaultWrapper
   # NOTE: for nvidia, the --impure option must be passed to home-manager switch
-  nixGL.defaultWrapper = "nvidia"; # options one of "mesa", "mesaPrime", "nvidia", "nvidiaPrime"
-  nixGL.installScripts = [ "nvidia" ];
+  nixGL.defaultWrapper = "mesa"; # options one of "mesa", "mesaPrime", "nvidia", "nvidiaPrime"
+  nixGL.installScripts = [ "mesa" ];
 }
