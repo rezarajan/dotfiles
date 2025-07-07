20a941c0cb211d1e6f8acb64fc705022ac687d07
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2025-07-07 10:23:48 +0000

    (fix) formatting in zellij kdl file - somehow fixes zjstatus not loading

--- home-manager/dotfiles/zellij/default.nix
+++ home-manager/dotfiles/zellij/default.nix
@@ -2,7 +2,7 @@
 
 {
   home.file.".config/zellij/config.kdl".source = ./config.kdl;
-  home.file.".config/zellij/layouts/default.kdl".source = 
+  home.file.".config/zellij/layouts/default.kdl".source =
     pkgs.replaceVars ./layouts/default.kdl{
       zjstatus = pkgs.lib.getExe pkgs.zjstatus;
     };
