fdd288c6f317ebeffbdacdfe235e1840f054cfff
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2025-11-02 08:44:33 -0500

    feat(nvim): add Python venv auto-detection for proper LSP and linting support

---
+++ nvim/lua/custom/plugins/python-venv.lua
@@ -0,0 +1,42 @@
+-- Python virtual environment support
+-- Automatically detect and activate venv for proper LSP/linting
+
+return {
+  'linux-cultist/venv-selector.nvim',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-telescope/telescope.nvim',
+    'mfussenegger/nvim-dap-python',
+  },
+  branch = 'regexp',
+  opts = {
+    -- Auto select venv when entering Python files
+    auto_refresh = true,
+    
+    -- Search patterns for finding venvs
+    name = {
+      'venv',
+      '.venv',
+      'env',
+      '.env',
+      'virtualenv',
+    },
+    
+    -- Path patterns to search for venvs
+    path = '.',
+    
+    -- Parents to check (search up directory tree)
+    parents = 2,
+    
+    -- Enable fd for faster searching
+    fd_binary_name = 'fd',
+    
+    -- Notify when venv is activated
+    notify_user_on_activate = true,
+  },
+  keys = {
+    { '<leader>vs', '<cmd>VenvSelect<cr>', desc = '[V]env [S]elect' },
+    { '<leader>vc', '<cmd>VenvSelectCached<cr>', desc = '[V]env [C]ached' },
+  },
+  ft = 'python',
+}
