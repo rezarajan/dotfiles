a8b906f40e5b3c352b54edf10432f21ae5ad7713
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2026-01-03 15:11:21 -0400

    update(home-manager): use new 25.11 version

--- home-manager/nixgl.nix
+++ home-manager/nixgl.nix
@@ -5,8 +5,8 @@ let
     packages = nixgl.packages;
     # see: https://mynixos.com/home-manager/option/nixGL.defaultWrapper
     # NOTE: for nvidia, the --impure option must be passed to home-manager switch
-    defaultWrapper = "nvidia"; # "mesa" | "mesaPrime" | "nvidia" | "nvidiaPrime"
-    installScripts = [ "nvidia" ];
+    defaultWrapper = "mesa"; # "mesa" | "mesaPrime" | "nvidia" | "nvidiaPrime"
+    installScripts = [ "mesa" ];
   };
 in {
   nixpkgs.config.allowUnfree = true;
