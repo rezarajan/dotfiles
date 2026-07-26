7521ffe0af11907b4080c60afa20c32e62ec043e
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2026-07-26 17:40:49 -0400

    update(ghostty): use GTK toolbar style - nvim: use gruvbox theme

--- nvim/lua/custom/plugins/auto-dark-mode.lua
+++ nvim/lua/custom/plugins/auto-dark-mode.lua
@@ -6,11 +6,11 @@ return {
     opts = {
       set_dark_mode = function()
         vim.api.nvim_set_option_value('background', 'dark', {})
-        colors.apply 'catppuccin-frappe'
+        colors.apply 'gruvbox'
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value('background', 'light', {})
-        colors.apply 'catppuccin-latte'
+        colors.apply 'gruvbox'
       end,
       update_interval = 3000,
       fallback = 'dark',
